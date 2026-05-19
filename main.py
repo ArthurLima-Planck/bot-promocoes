9b4cfda07df35a2d412dfc42a7ef41d6ee8de4e7
ArthurLima-Planck <arthurdev.2341@gmail.com>
2026-05-19 13:17:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import json
+
+from database import criar_tabelas
+from telegram_alert import enviar_telegram
+
+
 def main():
     criar_tabelas()
     enviar_telegram("TESTE TELEGRAM")
     print("Finalizado")
+
+
+if __name__ == "__main__":
+    main()
