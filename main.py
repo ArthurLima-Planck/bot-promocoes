82e2b70a1f8df3f4cead8d8bb7d7f3ad7185efa3
ArthurLima-Planck <arthurdev.2341@gmail.com>
2026-05-19 13:01:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def main():
-    criar_tabelas()
+import os
+import requests
 
-    enviar_telegram("🚀 TESTE: bot funcionando no GitHub")
 
-    verificar_produtos()
+def enviar_telegram(mensagem):
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
-    print("Finalizado")
+    print("TOKEN existe?", bool(token))
+    print("CHAT_ID existe?", bool(chat_id))
+    print("CHAT_ID:", chat_id)
+
+    if not token:
+        print("ERRO: TELEGRAM_TOKEN não configurado")
+        return
+
+    if not chat_id:
+        print("ERRO: TELEGRAM_CHAT_ID não configurado")
+        return
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    response = requests.post(
+        url,
+        data={
+            "chat_id": chat_id,
+            "text": mensagem
+        }
+    )
+
+    print("Status Telegram:", response.status_code)
+    print("Resposta Telegram:", response.text)
