afb51526f4ee43c07e695862078f4c3dcdf8f14b
ArthurLima-Planck <arthurdev.2341@gmail.com>
2026-05-19 13:07:42 -0300

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -6,19 +6,15 @@ def enviar_telegram(mensagem):
     token = os.getenv("TELEGRAM_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
-    if not token or not chat_id:
-        print("Telegram não configurado")
-        return
-
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
     response = requests.post(
         url,
         data={
             "chat_id": chat_id,
-            "text": mensagem,
-            "parse_mode": "HTML"
+            "text": mensagem
         }
     )
 
-    print(response.text)
+    print("STATUS:", response.status_code)
+    print("RESPOSTA:", response.text)
